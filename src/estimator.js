021876bf3c7940a164c545fa0eb90a0c220d7359
nonnyboing <nonnyboing@gmail.com>
2020-04-09 00:36:30 +0100

    completed challenge 1

--- src/estimator.js
+++ src/estimator.js
@@ -1,3 +1,20 @@
-const covid19ImpactEstimator = (data) => data;
+const covid19ImpactEstimator = (data) => {
+  // const input = data;
+  const { reportedCases } = data;
+  const impact = {};
+  const severeImpact = {};
+
+  impact.currentlyInfected = reportedCases * 10;
+  impact.infectionsByRequestedTime = impact.currentlyInfected * 512;
+
+  severeImpact.currentlyInfected = reportedCases * 50;
+  severeImpact.infectionsByRequestedTime = severeImpact.currentlyInfected * 512;
+
+  return {
+    input: data,
+    Impact: impact,
+    SevereImpact: severeImpact
+  };
+};
 
 export default covid19ImpactEstimator;
