8da170f4208a59727a59ba6d5164fe9656584ea6
nonnyboing <nonnyboing@gmail.com>
2020-04-10 16:50:24 +0100

    submit challenge 3

--- src/estimator.js
+++ src/estimator.js
@@ -1,6 +1,6 @@
 const covid19ImpactEstimator = (data) => {
   const {
-    reportedCases, periodType, timeToElapse, totalHospitalBeds
+    reportedCases, periodType, timeToElapse, totalHospitalBeds, region
   } = data;
   const impact = {};
   const severeImpact = {};
@@ -17,18 +17,35 @@ const covid19ImpactEstimator = (data) => {
 
   const factor = Math.trunc(days / 3);
   const beds = totalHospitalBeds * 0.35;
+  const avgIncome = region.avgDailyIncomeInUSD;
+  const avgPopulation = region.avgDailyIncomePopulation;
 
   impact.currentlyInfected = reportedCases * 10;
   impact.infectionsByRequestedTime = impact.currentlyInfected * (2 ** factor);
   impact.severeCasesByRequestedTime = Math.trunc(impact.infectionsByRequestedTime * 0.15);
   impact.hospitalBedsByRequestedTime = Math.ceil(beds - impact.severeCasesByRequestedTime);
+  impact.casesForICUByRequestedTime = Math.trunc(impact.infectionsByRequestedTime * 0.05);
+  impact.casesForVentilatorsByRequestedTime = Math.trunc(impact.infectionsByRequestedTime * 0.02);
+  const impDollarsInFlight = impact.infectionsByRequestedTime * avgIncome * avgPopulation * days;
+  impact.dollarsInFlight = impDollarsInFlight.toFixed(2);
 
   severeImpact.currentlyInfected = reportedCases * 50;
   severeImpact.infectionsByRequestedTime = severeImpact.currentlyInfected * (2 ** factor);
+
   const severeCases = severeImpact.infectionsByRequestedTime * 0.15;
   severeImpact.severeCasesByRequestedTime = Math.trunc(severeCases);
+
   const availableBeds = beds - severeImpact.severeCasesByRequestedTime;
   severeImpact.hospitalBedsByRequestedTime = Math.ceil(availableBeds);
+
+  const casesForICU = severeImpact.infectionsByRequestedTime * 0.05;
+  severeImpact.casesForICUByRequestedTime = Math.trunc(casesForICU);
+
+  const casesForVentilators = severeImpact.infectionsByRequestedTime * 0.02;
+  severeImpact.casesForVentilatorsByRequestedTime = Math.trunc(casesForVentilators);
+
+  const dollarsInFlight = severeImpact.infectionsByRequestedTime * avgIncome * avgPopulation * days;
+  severeImpact.dollarsInFlight = dollarsInFlight.toFixed(2);
 
   return {
     data,
