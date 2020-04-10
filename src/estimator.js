397554df1ff44d6cd10de2f8b5100227c32803fb
nonnyboing <nonnyboing@gmail.com>
2020-04-10 13:34:39 +0100

    updated challenge 1

--- src/estimator.js
+++ src/estimator.js
@@ -1,14 +1,25 @@
 const covid19ImpactEstimator = (data) => {
-  // const input = data;
-  const { reportedCases } = data;
+  const { reportedCases, periodType, timeToElapse } = data;
   const impact = {};
   const severeImpact = {};
 
+  let days;
+
+  if (periodType === 'months') {
+    days = timeToElapse * 30;
+  } else if (periodType === 'weeks') {
+    days = timeToElapse * 7;
+  } else {
+    days = timeToElapse;
+  }
+
+  const factor = Math.trunc(days / 3);
+
   impact.currentlyInfected = reportedCases * 10;
-  impact.infectionsByRequestedTime = impact.currentlyInfected * 512;
+  impact.infectionsByRequestedTime = impact.currentlyInfected * (2 ** factor);
 
   severeImpact.currentlyInfected = reportedCases * 50;
-  severeImpact.infectionsByRequestedTime = severeImpact.currentlyInfected * 512;
+  severeImpact.infectionsByRequestedTime = severeImpact.currentlyInfected * (2 ** factor);
 
   return {
     input: data,
